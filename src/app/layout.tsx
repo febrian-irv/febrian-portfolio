9eb23d3c252a867d961cc420a7d28892a12c794e
febrian-irv <123155363+febrian-irv@users.noreply.github.com>
2025-01-06 00:47:55 +0700

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main className="container mx-auto px-4 py-6">{children}</main>
+      </body>
+    </html>
+  );
+}
